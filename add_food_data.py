25e2fa2145a0e7d762c6a710053894531ead98ed
Ivan <129706771+0Medsok0@users.noreply.github.com>
2024-12-06 21:43:57 +0700

    Add files via upload
    updating the index.py and add_food_data.py

--- add_food_data.py
+++ add_food_data.py
@@ -4,31 +4,26 @@ from index import app, db, Food, Store
 # Добавляем первый магазин
 def add_food_data():
     with app.app_context():
-        # Создаем таблицы, если они не существуют
         db.create_all()
-
-        # Удаляем все существующие записи (если нужно)
         db.session.query(Food).delete()
         db.session.query(Store).delete()
         db.session.commit()
 
-        # Создаем магазин
         store = Store(name="Вкусняшкин", description="Лучший магазин еды в городе", image_path="images/store_1.jpg")
         db.session.add(store)
         db.session.commit()
 
-        # Добавляем товары в магазин
         foods = [
             Food(name="Пицца", description="Томатный соус, моцарелла, пепперони", delivery_time="30 минут",
-                 image_path="images/pizza.jpg", store_id=store.id),
+                 image_path="images/pizza.jpg", store_id=store.id, category='fast_food'),
             Food(name="Бургер", description="Котлета из говядины, листья салата, помидор, сыр",
-                 delivery_time="20 минут", image_path="images/burger.jpg", store_id=store.id),
+                 delivery_time="20 минут", image_path="images/burger.jpg", store_id=store.id, category='fast_food'),
             Food(name="Суши", description="Рис, сырая рыба, нори", delivery_time="45 минут",
-                 image_path="images/sushi.jpg", store_id=store.id),
+                 image_path="images/sushi.jpg", store_id=store.id, category='fast_food'),
             Food(name="Салат", description="Смесь зелени, помидоров черри, огурцов, сыра фета",
-                 delivery_time="15 минут", image_path="images/salad.jpg", store_id=store.id),
+                 delivery_time="15 минут", image_path="images/salad.jpg", store_id=store.id, category='fruits_vegetables'),
             Food(name="Паста", description="Спагетти, томатный соус, фрикадельки", delivery_time="25 минут",
-                 image_path="images/pasta.jpg", store_id=store.id)
+                 image_path="images/pasta.jpg", store_id=store.id, category='fast_food')
         ]
 
         for food in foods:
@@ -37,50 +32,41 @@ def add_food_data():
         db.session.commit()
         print("Данные добавлены успешно!")
 
-
 # Добавляем второй магазин
 def add_food_data_2():
     with app.app_context():
-        # Создаем таблицы, если они не существуют
         db.create_all()
 
-        # Удаляем все существующие записи (если нужно)
-        # db.session.query(Food).delete()
-        # db.session.query(Store).delete()
-        # db.session.commit()
-
-        # Создаем магазин
         store = Store(name="Лавка вкуса", description="Мы специализируемся на продаже эксклюзивных "
                                                       "продуктов, таких как авторские сыры, редкие специи, "
                                                       "необычные десерты и напитки", image_path="images/store_2.jpg")
         db.session.add(store)
         db.session.commit()
 
-        # Добавляем товары в магазин
         foods = [
             Food(name="Сыр с плесенью", description="Изысканный продукт с благородной голубой плесенью, который "
                                                     "обладает насыщенным вкусом и ароматом. Идеально подходит для "
                                                     "гурманов и любителей необычных сыров", delivery_time="30-45 "
                                                                                                           "минут",
-                 image_path="images/cheese_with_mold.jpg", store_id=store.id),
+                 image_path="images/cheese_with_mold.jpg", store_id=store.id, category='dairy'),
             Food(name="Десерт шотландский", description="Традиционный шотландский десерт, состоящий из песочного "
                                                         "теста, свежих ягод и нежного сливочного крема. Обладает "
                                                         "ярким вкусом и неповторимым ароматом", delivery_time="20-35 "
                                                                                                               "минут",
-                 image_path="images/Dessert_is_Scottish.jpg", store_id=store.id),
+                 image_path="images/Dessert_is_Scottish.jpg", store_id=store.id, category='dairy'),
             Food(name="Горный Мохито", description="Освежающий напиток, приготовленный на основе горного чая с "
                                                    "добавлением мяты, лайма и сахара. Отлично утоляет жажду и "
                                                    "бодрит", delivery_time="45-1 час",
                  image_path="images/Mountain_Mojito.jpg",
-                 store_id=store.id),
+                 store_id=store.id, category='drinks'),
             Food(name="Сыр голландский", description="Твёрдый голландский сыр, известный своим насыщенным вкусом и "
                                                      "ароматом. Идеально подходит для приготовления различных блюд и "
                                                      "закусок", delivery_time="25 минут",
-                 image_path="images/Dutch_cheese.jpg", store_id=store.id),
+                 image_path="images/Dutch_cheese.jpg", store_id=store.id, category='dairy'),
             Food(name="Копченая паприка", description="Ароматная копчёная приправа, изготовленная из красного перца и "
                                                       "других специй. Обладает ярким вкусом и ароматом, "
                                                       "идеально подходит для мясных и овощных блюд",
-                 delivery_time="35 минут", image_path="images/Smoked_paprika.jpg", store_id=store.id)
+                 delivery_time="35 минут", image_path="images/Smoked_paprika.jpg", store_id=store.id, category='groceries')
         ]
 
         for food in foods:
@@ -89,26 +75,18 @@ def add_food_data_2():
         db.session.commit()
         print("Данные добавлены успешно!")
 
-
 # Добавляем третий магазин
 def add_food_data_3():
     with app.app_context():
-        # Создаем таблицы, если они не существуют
         db.create_all()
 
-        # Удаляем все существующие записи (если нужно)
-        # db.session.query(Food).delete()
-        # db.session.query(Store).delete()
-        # db.session.commit()
-
-        # Создаем магазин
         store = Store(name="Гастровкус", description="Мы предлагаем широкий ассортимент товаров, включая свежие овощи "
                                                      "и фрукты, мясные и молочные продукты, бакалею и кондитерские "
-                                                     "изделия", image_path="images/store_2.jpg")
+                                                     "изделия", image_path="images/store_3.jpg")
         db.session.add(store)
         db.session.commit()
+        print(f"Магазин добавлен: {store.name}")
 
-        # Добавляем товары в магазин
         foods = [
             Food(name="Помидоры тепличные", description="Эти помидоры выращиваются в специальных теплицах, "
                                                         "где создаются оптимальные условия для роста и развития "
@@ -118,7 +96,7 @@ def add_food_data_3():
                                                         "различных блюд и консервации",
                  delivery_time="50 минут",
                  image_path="images/Greenhouse_tomatoes.jpg",
-                 store_id=store.id),
+                 store_id=store.id, category='fruits_vegetables'),
             Food(name="Дыня абхазкая", description="Абхазская дыня известна своими крупными размерами, сладким вкусом "
                                                    "и ароматом. Она выращивается в Абхазии и считается одной из "
                                                    "лучших дынь в мире. Плоды имеют жёлтую кожуру и белую мякоть, "
@@ -126,7 +104,7 @@ def add_food_data_3():
                                                    "виде, а также используют для приготовления десертов и напитков",
                  delivery_time="50 минут",
                  image_path="images/The_Abkhazian_melon.jpg",
-                 store_id=store.id),
+                 store_id=store.id, category='fruits_vegetables'),
             Food(name="Молоко фермерское", description="Фермерское молоко производится из молока коров, которые "
                                                        "питаются свежей травой и сеном. Оно имеет приятный вкус и "
                                                        "запах, а также богато витаминами и минералами. Фермерское "
@@ -134,31 +112,115 @@ def add_food_data_3():
                                                        "продуктов, таких как йогурт, творог и сметана",
                  delivery_time="50 минут",
                  image_path="images/Farm_milk.jpg",
-                 store_id=store.id),
+                 store_id=store.id, category='dairy'),
             Food(name="Гречка", description="Гречка — это полезный и питательный злак, который выращивается во многих "
                                             "странах мира. Она богата железом, магнием и другими полезными "
                                             "веществами. Гречку используют для приготовления каш, гарниров и других "
                                             "блюд",
                  delivery_time="50 минут",
                  image_path="images/Buckwheat.jpg",
-                 store_id=store.id),
+                 store_id=store.id, category='groceries'),
             Food(name="Печенье из песочного теста",
                  description="Песочное печенье изготавливается из муки, масла, сахара и яиц. Оно имеет рассыпчатую "
                              "структуру и нежный вкус. Песочное печенье часто посыпают сахарной пудрой или украшают "
                              "глазурью",
                  delivery_time="50 минут",
                  image_path="images/Shortbread_cookies.jpg",
-                 store_id=store.id)
+                 store_id=store.id, category='groceries')
         ]
 
         for food in foods:
             db.session.add(food)
+            print(f"Товар добавлен: {food.name}")
 
         db.session.commit()
         print("Данные добавлены успешно!")
 
+# Добавляем четвертый магазин
+def add_food_data_4():
+    with app.app_context():
+        db.create_all()
 
+        store = Store(name="Хочу Пури", description="Вкус традиций в каждом кусочке!", image_path="images/store_4.jpg")
+        db.session.add(store)
+        db.session.commit()
+        print(f"Магазин добавлен: {store.name}")
 
+        foods = [
+            Food(name="Хинкали", description="Традиционные грузинские пельмени с сочной начинкой из мяса и специями",
+                 delivery_time="50 минут",
+                 image_path="images/Khinkali.jpg",
+                 store_id=store.id, category='georgian_cuisine'),
+            Food(name="Лобио", description="Грузинское блюдо из фасоли, приготовленное с овощами, специями и зеленью",
+                 delivery_time="50 минут",
+                 image_path="images/Lobio.jpg",
+                 store_id=store.id, category='georgian_cuisine'),
+            Food(name="Чахохбили", description="Тушеное мясо (обычно курица) с помидорами, луком, чесноком и грузинскими специями",
+                 delivery_time="50 минут",
+                 image_path="images/Chakhokhbili.jpg",
+                 store_id=store.id, category='georgian_cuisine'),
+            Food(name="Манты", description="Традиционные центральноазиатские пельмени с сочной начинкой из мяса, лука и специй, приготовленные на пару",
+                 delivery_time="50 минут",
+                 image_path="images/Mantas.jpg",
+                 store_id=store.id, category='georgian_cuisine'),
+            Food(name="Хочапури",
+                 description="Грузинские лепешки с начинкой из сыра, яйца и масла, выпекаемые до золотистой корочки",
+                 delivery_time="50 минут",
+                 image_path="images/Khachapuri.jpg",
+                 store_id=store.id, category='georgian_cuisine')
+        ]
+
+        for food in foods:
+            db.session.add(food)
+            print(f"Товар добавлен: {food.name}")
+
+        db.session.commit()
+        print("Данные добавлены успешно!")
+
+# Добавляем пятый магазин
+def add_food_data_5():
+    with app.app_context():
+        db.create_all()
+
+        store = Store(name="Лента", description="Качество и свежесть в каждом продукте", image_path="images/store_5.jpg")
+        db.session.add(store)
+        db.session.commit()
+        print(f"Магазин добавлен: {store.name}")
+
+        foods = [
+            Food(name="Свежие овощи и фрукты", description="Всегда свежие и сезонные овощи и фрукты, выращенные с любовью",
+                 delivery_time="50 минут",
+                 image_path="images/Fresh_vegetables_and_fruits.jpg",
+                 store_id=store.id, category='fruits_vegetables'),
+            Food(name="Молочные продукты", description="Широкий ассортимент молока, йогуртов, сыров и других молочных продуктов высокого качества",
+                 delivery_time="50 минут",
+                 image_path="images/Dairy_products.jpg",
+                 store_id=store.id, category='dairy'),
+            Food(name="Мясные деликатесы", description="Выбор свежего мяса и колбасных изделий для любого вкуса и бюджета",
+                 delivery_time="50 минут",
+                 image_path="images/Meat_delicacies.jpg",
+                 store_id=store.id, category='groceries'),
+            Food(name="Хлебобулочные изделия", description="Свежеиспеченный хлеб, булочки и выпечка, приготовленные по традиционным рецептам",
+                 delivery_time="50 минут",
+                 image_path="images/Bakery_products.jpg",
+                 store_id=store.id, category='groceries'),
+            Food(name="Натуральные соки",
+                 description="Соки без добавок и консервантов, идеальные для здорового питания",
+                 delivery_time="50 минут",
+                 image_path="images/Natural_juices.jpg",
+                 store_id=store.id, category='drinks')
+        ]
+
+        for food in foods:
+            db.session.add(food)
+            print(f"Товар добавлен: {food.name}")
+
+        db.session.commit()
+        print("Данные добавлены успешно!")
 
 if __name__ == '__main__':
+    add_food_data()
+    add_food_data_2()
     add_food_data_3()
+    add_food_data_4()
+    add_food_data_5()
--- index.py
+++ index.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, url_for, request, jsonify
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///food.db'
+# Настройки для Flask-Mail
+app.config['MAIL_SERVER'] = 'smtp.mail.ru'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'jhon.programmer@bk.ru'
+app.config['MAIL_PASSWORD'] = 'ZVRwmTKgXXNYYJrmm3tc'
+
 db = SQLAlchemy(app)
+mail = Mail(app)
 
 class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,12 +23,14 @@ class Food(db.Model):
     delivery_time = db.Column(db.String(50), nullable=False)
     image_path = db.Column(db.String(200), nullable=True)
     store_id = db.Column(db.Integer, db.ForeignKey('store.id'), nullable=False)
-
+    category = db.Column(db.String(50), nullable = False)
+    
 class Store(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     image_path = db.Column(db.String(200), nullable=True)
+    foods = db.relationship('Food', backref='store', lazy=True)
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,11 +38,13 @@ class Order(db.Model):
     order_date = db.Column(db.DateTime, default=datetime.utcnow)
     delivery_cost = db.Column(db.Float, nullable=False)
 
-
 @app.route('/')
 def home():
     stores = Store.query.all()
-    return render_template('home.html', stores=stores)
+    foods = Food.query.all()
+    print(f"Количество товаров: {len(foods)}")
+    print(f"Количество магазинов: {len(stores)}")
+    return render_template('home.html', stores=stores, foods=foods)
 
 @app.route('/store/<int:store_id>')
 def store_detail(store_id):
@@ -64,6 +78,36 @@ def about():
 @app.route('/vacancies')
 def vacancies():
     return render_template('vacancies.html')
+
+@app.route('/apply')
+def apply():
+    return render_template('resume.html')
+
+@app.route('/resume', methods=['GET', 'POST'])
+def resume():
+    if request.method == 'POST':
+        # Обработка данных формы
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        # Отправка данных по электронной почте
+        send_email(name, email, message)
+        return redirect(url_for('apply'))
+    return render_template('resume.html')
+
+def send_email(name, email, message):
+    receiver_email = "jhon.programmer@bk.ru"
+    subject = "New Resume Submission"
+    body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+
+    msg = Message(subject, sender=email, recipients=[receiver_email])
+    msg.body = body
+
+    try:
+        mail.send(msg)
+    except Exception as e:
+        print(f'Ошибка отправки email {e}')
+
 
 if __name__ == '__main__':
     with app.app_context():
